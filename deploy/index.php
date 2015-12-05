ecc16db71486b15bf8b06dbcd3a6c65d20ef04ab
Kevin Chappell <kevin.b.chappell@gmail.com>
2015-12-05 14:22:31 +0000

    initial commit

---
+++ deploy/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<script>
+		  ((window.gitter = {}).chat = {}).options = {
+		    room: 'anthonymvpr/finalproject'
+		  };
+		</script>
+	<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>
+	</body>
+</html>
